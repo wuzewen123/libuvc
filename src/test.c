973e2bf4113b4b012b5b93597a16685c1f4a291e
Wuzw <1207837538@qq.com>
2024-03-21 10:47:03 +0800

    get depthamp from module

--- src/test.c
+++ src/test.c
@@ -37,39 +37,7 @@
 #include "libuvc/libuvc.h"
 
 void cb(uvc_frame_t *frame, void *ptr) {
-  uvc_frame_t *bgr;
-  uvc_error_t ret;
-  IplImage* cvImg;
-
   printf("callback! length = %u, ptr = %d\n", frame->data_bytes, (int) ptr);
-
-  bgr = uvc_allocate_frame(frame->width * frame->height * 3);
-  if (!bgr) {
-    printf("unable to allocate bgr frame!");
-    return;
-  }
-
-  ret = uvc_any2bgr(frame, bgr);
-  if (ret) {
-    uvc_perror(ret, "uvc_any2bgr");
-    uvc_free_frame(bgr);
-    return;
-  }
-
-  cvImg = cvCreateImageHeader(
-      cvSize(bgr->width, bgr->height),
-      IPL_DEPTH_8U,
-      3);
-
-  cvSetData(cvImg, bgr->data, bgr->width * 3); 
-
-  cvNamedWindow("Test", CV_WINDOW_AUTOSIZE);
-  cvShowImage("Test", cvImg);
-  cvWaitKey(10);
-
-  cvReleaseImageHeader(&cvImg);
-
-  uvc_free_frame(bgr);
 }
 
 int main(int argc, char **argv) {
@@ -109,34 +77,12 @@ int main(int argc, char **argv) {
       res = uvc_get_stream_ctrl_format_size(
           devh, &ctrl, UVC_FRAME_FORMAT_YUYV, 640, 480, 30
       );
+      res = uvc_start_streaming(devh, &ctrl, cb, 12345, 0);
 
-      uvc_print_stream_ctrl(&ctrl, stderr);
+      sleep(10);
+      uvc_stop_streaming(devh);
+      puts("Done streaming.");
 
-      if (res < 0) {
-        uvc_perror(res, "get_mode");
-      } else {
-        res = uvc_start_streaming(devh, &ctrl, cb, 12345, 0);
-
-        if (res < 0) {
-          uvc_perror(res, "start_streaming");
-        } else {
-          puts("Streaming for 10 seconds...");
-          uvc_error_t resAEMODE = uvc_set_ae_mode(devh, 1);
-          uvc_perror(resAEMODE, "set_ae_mode");
-          int i;
-          for (i = 1; i <= 10; i++) {
-            /* uvc_error_t resPT = uvc_set_pantilt_abs(devh, i * 20 * 3600, 0); */
-            /* uvc_perror(resPT, "set_pt_abs"); */
-            uvc_error_t resEXP = uvc_set_exposure_abs(devh, 20 + i * 5);
-            uvc_perror(resEXP, "set_exp_abs");
-            
-            sleep(1);
-          }
-          sleep(10);
-          uvc_stop_streaming(devh);
-	  puts("Done streaming.");
-        }
-      }
 
       uvc_close(devh);
       puts("Device closed");
